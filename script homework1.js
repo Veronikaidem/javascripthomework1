5eb43cb0f7ab33aea8d9d7466c0d97d252cc5eb3
Veronikaidem <110629355+Veronikaidem@users.noreply.github.com>
2022-09-26 15:04:17 +0200

    Update script homework1.js

--- script homework1.js
+++ script homework1.js
@@ -1,4 +1,83 @@
-// ------homework=======
+============SECOND HOMEWORK========
+   // function sum(...numbers) {
+//     let numberSum = 0; 
+//     for(let item of numbers) 
+//      if (item > 0) {
+//         numberSum+=item;
+//        }
+//  return numberSum;
+//  }
+//  let result = sum(2, -9, 5, 11, -30, 100, 8, -11, -4, 5, -6, 8);
+//  console.log(result);
+
+
+// =====homework2=====
+
+// function sum(...numbers) {
+//     let numberSum = 0; 
+//     for(let item of numbers) {
+//      numberSum+=item;
+//     }
+//  return numberSum;
+//  }
+//  let result = sum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+//  console.log(result);
+
+
+
+
+// =====homework3=====
+
+// let user = {
+//     firstname: 'giorgi', 
+//     lastname: 'saakadze',
+//     age: 32,
+//     isLoggedIn: true
+// }
+
+// function namereturn(x, y) {
+//     return x + " " + y;
+// }
+
+// let newusername = namereturn(user.firstname, user.lastname);
+// console.log(newusername);
+
+
+
+
+// =====homework4=====
+
+
+
+
+
+
+// =====homework5=====
+
+// let array = [1,2,4,10,34,5,7,87];
+//  for(let item of array){
+//    if(0 < item && 10 > item){
+//        console.log(item);
+//     }
+// }
+
+
+// =====homework6=======
+
+// let numbers =[1,2,3,4,5,6,7,8,9,10];
+//  for(let item of numbers){
+// if(5 == item){
+//      console.log(item);
+//      }
+//     }
+
+
+
+
+
+
+
+// ------homework1=======
 
 // let user = {
 //     firstname: 'giorgi',
@@ -86,3 +165,5 @@ for (let item of numbers){
         { username: 'anna', status:'true'},
     ];
     console.log(users[2].status);
+
+
